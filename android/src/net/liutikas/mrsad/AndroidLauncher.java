6a7581e720d429f8e42055aead05eba221117d28
Aurimas Liutikas <aurimas@liutikas.net>
2016-06-22 19:38:44 -0700

    Draw lonely Mr.Sad.

--- android/src/net/liutikas/mrsad/AndroidLauncher.java
+++ android/src/net/liutikas/mrsad/AndroidLauncher.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import net.liutikas.mrsad.MrSadGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MrSadGame(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MrSadGame(), config);
+    }
 }
