13413f3a22736868ffbf11c3b8b6e94528f86e49
Aurimas Liutikas <aurimas@liutikas.net>
2016-06-22 22:30:43 -0700

    Limit jump duration.

--- core/src/net/liutikas/mrsad/Constants.java
+++ core/src/net/liutikas/mrsad/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
     public static final float WORLD_SIZE = 128f;
 
     public static final float PLAYER_WALK_SPEED = 80f;
-    public static final float PLAYER_JUMP_SPEED = 80f;
+    public static final float PLAYER_JUMP_SPEED = 120f;
+    public static final float PLAYER_MAX_JUMP_DURATION = 0.3f;
+    public static final float GRAVITY_ACCELERATION = 500f;
 }
--- core/src/net/liutikas/mrsad/entities/Player.java
+++ core/src/net/liutikas/mrsad/entities/Player.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import net.liutikas.mrsad.Constants;
@@ -15,6 +17,9 @@ import net.liutikas.mrsad.Constants;
 public class Player {
     private final Viewport mViewport;
     private Vector2 mPosition;
+    private Vector2 mVelocity;
+    private JumpState mJumpState;
+    private long mJumpStartTime;
     private Texture mTexture;
 
     public Player(Viewport viewport) {
@@ -23,23 +28,62 @@ public class Player {
     }
 
     public void init() {
-        mPosition = new Vector2(mViewport.getWorldWidth() / 2, 0);
+        mPosition = new Vector2(mViewport.getWorldWidth() / 2, 40);
+        mVelocity = new Vector2(0, 0);
+        mJumpState = JumpState.FALLING;
     }
 
     public void update(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            mPosition.x -= delta * Constants.PLAYER_WALK_SPEED;
+            moveLeft(delta);
         } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            mPosition.x += delta * Constants.PLAYER_WALK_SPEED;
+            moveRight(delta);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            mPosition.y += delta * Constants.PLAYER_JUMP_SPEED;
+            if (mJumpState == JumpState.GROUNDED) {
+                mJumpState = JumpState.JUMPING;
+                mJumpStartTime = TimeUtils.nanoTime();
+            }
+            float jumpDuration = MathUtils.nanoToSec * (TimeUtils.nanoTime() - mJumpStartTime);
+            if (jumpDuration < Constants.PLAYER_MAX_JUMP_DURATION
+                    && mJumpState == JumpState.JUMPING) {
+                mPosition.y += delta * Constants.PLAYER_JUMP_SPEED;
+            } else {
+                continueFalling(delta);
+            }
         } else if (mPosition.y > 0) {
-            mPosition.y -= delta * 80;
+            continueFalling(delta);
         }
     }
 
     public void render(SpriteBatch batch) {
         batch.draw(mTexture, mPosition.x, mPosition.y);
+    }
+
+    private void moveLeft(float delta) {
+        mPosition.x -= delta * Constants.PLAYER_WALK_SPEED;
+    }
+
+    private void moveRight(float delta) {
+        mPosition.x += delta * Constants.PLAYER_WALK_SPEED;
+    }
+
+    private void continueFalling(float delta) {
+        if (mJumpState == JumpState.JUMPING) {
+            mJumpState = JumpState.FALLING;
+        }
+        mVelocity.y -= delta * Constants.GRAVITY_ACCELERATION;
+        mPosition.y += delta * mVelocity.y;
+        if (mPosition.y <= 0) {
+            mPosition.y = 0f;
+            mVelocity.y = 0f;
+            mJumpState = JumpState.GROUNDED;
+        }
+    }
+
+    enum JumpState {
+        GROUNDED, // No vertical velocity. Player is on a ground.
+        JUMPING,  // Positive up velocity. Can stay in this state for up to PLAYER_MAX_JUMP_DURATION.
+        FALLING   // Negative vertical velocity. Changes to GROUNDED when player reaches ground.
     }
 }
