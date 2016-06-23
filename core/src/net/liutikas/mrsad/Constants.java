485c227e5c6ef8a55f88e5d86b1f35cd012a431c
Aurimas Liutikas <aurimas@liutikas.net>
2016-06-22 19:48:21 -0700

    Add a basic game screen.

---
+++ core/src/net/liutikas/mrsad/Constants.java
@@ -0,0 +1,5 @@
+package net.liutikas.mrsad;
+
+public class Constants {
+    public static final float WORLD_SIZE = 128f;
+}
--- core/src/net/liutikas/mrsad/GameScreen.java
+++ core/src/net/liutikas/mrsad/GameScreen.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /**
  * The main game screen responsible for handling the game play.
  */
 public class GameScreen implements Screen {
+    private ExtendViewport mViewport;
+
     SpriteBatch batch;
     Texture img;
 
@@ -17,20 +20,23 @@ public class GameScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
         img = new Texture("player-right.png");
+        mViewport = new ExtendViewport(Constants.WORLD_SIZE, Constants.WORLD_SIZE);
     }
 
     @Override
     public void render(float delta) {
+        mViewport.apply();
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(mViewport.getCamera().combined);
         batch.begin();
-        batch.draw(img, 0, 0, 100, 100);
+        batch.draw(img, 0, 0);
         batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        mViewport.update(width, height, true);
     }
 
     @Override
