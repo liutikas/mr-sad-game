bcfb2e1e5ff04844f8562cc8cf11c886d8e62d78
Aurimas Liutikas <aurimas@liutikas.net>
2016-06-22 19:22:20 -0700

    Initialize empty LibGDX project.

---
+++ desktop/src/net/liutikas/mrsad/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.liutikas.mrsad.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.liutikas.mrsad.MrSadGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MrSadGame(), config);
+	}
+}
