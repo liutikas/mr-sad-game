d1298f531eed576cdaad55b788b60aeadbc1dc71
Aurimas Liutikas <aurimas@liutikas.net>
2016-06-22 21:58:22 -0700

    Add basic player movement and jump controls.

--- core/src/net/liutikas/mrsad/Constants.java
+++ core/src/net/liutikas/mrsad/Constants.java
@@ -2,4 +2,7 @@ package net.liutikas.mrsad;
 
 public class Constants {
     public static final float WORLD_SIZE = 128f;
+
+    public static final float PLAYER_WALK_SPEED = 80f;
+    public static final float PLAYER_JUMP_SPEED = 80f;
 }
--- core/src/net/liutikas/mrsad/entities/Player.java
+++ core/src/net/liutikas/mrsad/entities/Player.java
@@ -1,10 +1,13 @@
 package net.liutikas.mrsad.entities;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import net.liutikas.mrsad.Constants;
 
 /**
  * The main game character the the player will be controlling.
@@ -24,10 +27,19 @@ public class Player {
     }
 
     public void update(float delta) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            mPosition.x -= delta * Constants.PLAYER_WALK_SPEED;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            mPosition.x += delta * Constants.PLAYER_WALK_SPEED;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            mPosition.y += delta * Constants.PLAYER_JUMP_SPEED;
+        } else if (mPosition.y > 0) {
+            mPosition.y -= delta * 80;
+        }
     }
 
     public void render(SpriteBatch batch) {
-        Gdx.app.log("Player", "Position " + mViewport.getWorldWidth());
         batch.draw(mTexture, mPosition.x, mPosition.y);
     }
 }
